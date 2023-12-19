99846181b1bf70a3f68a4c36e7992b2e382e7ba0
Sanjumahi <gaddamsanjai545@gmail.com>
2023-12-19 11:51:06 +0000

    initial Commit

---
+++ xam/src/main/java/Treemap/xam/Treemap.java
@@ -0,0 +1,17 @@
+package Treemap.xam;
+import java.util.*;
+public abstract class Treemap {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Map <String,Integer> td = new TreeMap<String,Integer>();
+      td.put("Prabhas",10);
+      td.put("Dhoni", 07);
+      td.put("Sanjay", 37);
+      td.put("Siddu", 50);
+      
+     System.out.println(td);
+	}
+
+}
